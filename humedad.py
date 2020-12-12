ab72ff145e20935168be18d1db74a58be5ec2ed7
XavierEsquivel9593 <63012235+XavierEsquivel9593@users.noreply.github.com>
2020-12-11 18:08:39 -0600

    Update humedad.py

--- humedad.py
+++ humedad.py
@@ -15,7 +15,6 @@ from pyspark.sql.types import StringType, StructType, StructField
 r = requests.get('http://144.202.34.148:4002/api/sensor')
 #alamacena los datos en la variable
 Respuesta = r.text
-#print("toda",Respuesta)
 
 spark = SparkSession \
     .builder \
@@ -25,9 +24,9 @@ spark = SparkSession \
 #contexto de spark representando la conexion al cluster spark
 sc = spark.sparkContext
 
-#convertir Json a dataframe
+#convertir Json obtenido del servidor de la api a dataframe
 jsonStrings = [Respuesta]
-#se crea un RDD en base a la coleccion de datos existentes con parallelize
+#se crea un RDD(colecicon de elementos) en base a la coleccion de datos existentes con parallelize
 otherPeopleRDD = sc.parallelize(jsonStrings)
 otherPeople = spark.read.json(otherPeopleRDD)
 
@@ -35,7 +34,7 @@ otherPeople = spark.read.json(otherPeopleRDD)
 otherPeople.printSchema()
 otherPeople.createOrReplaceTempView("humedad")
 #obtener los datos de la tabla dentro del rango de fecha
-spark.sql("select * from humedad where fecha between '2020-12-11T03:15:45.208Z' and '2020-12-11T03:16:08.673Z'").show
+spark.sql("select * from humedad where fecha between '2020-12-11T03:16:10.771Z' and '2020-12-11T23:35:50.068Z'").show()
 #obtener maxima, minima, media de humedad dentro del rango de fecha
-spark.sql("select max (humedad), min (humedad), avg (humedad) from humedad where fecha between '2020-12-11T03:15:45.208Z' and '2020-12-11T03:16:08.673Z'").show()
+spark.sql("select max (humedad), min (humedad), avg (humedad) from humedad where fecha between '2020-12-11T03:16:10.771Z' and '2020-12-11T23:35:50.068Z'").show()
 spark.stop()
